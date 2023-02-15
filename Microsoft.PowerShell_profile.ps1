0f564d85d38a283c2edec6db499778b8180985e6
Roy Vanegas <roy@thecodeeducators.com>
2023-02-14 22:22:36 -0500

    Add home alias

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -47,6 +47,7 @@ Set-Alias -Name dropbox -Value my_dropbox
 Set-Alias -Name open -Value ii
 Set-Alias -Name touch -Value touch_file
 Set-Alias -Name rm -Value rm-item
+Set-Alias home 'cd'
 
 # Refresh environment with `refreshenv`
 
